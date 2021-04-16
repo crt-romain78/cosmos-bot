8264ed72fd853fb6a345f4ee41fe3564c1219950
crt-romain78 <75182534+crt-romain78@users.noreply.github.com>
2021-04-16 17:47:50 +0200

    Update invite.js

--- commands/help/invite.js
+++ commands/help/invite.js
@@ -8,7 +8,7 @@ module.exports = {
     
     let embed = new discord.MessageEmbed()
     .setTitle(`HERE INVITE LINK OF BOT`)
-    .setDescription(`<a:Kk:792521917507829760>[CLICK HERE](https://discord.com/api/oauth2/authorize?client_id=825863213497319465&redirect_uri=https%3A%2F%2Fdsc.gg%2Fcrtbot%2F&response_type=code&scope=identify) OR [support server ](https://discord.com/oauth2/authorize?client_id=828285107696697404&redirect_uri=https%3A%2F%2Fdiscord.gg%2FVkpv7pPRMh&response_type=code&scope=identify) Oauth2 link to prevent raid`)
+    .setDescription(`<a:Kk:792521917507829760>[CLICK HERE](https://discord.com/api/oauth2/authorize?client_id=825863213497319465&redirect_uri=https%3A%2F%2Fdsc.gg%2Fcrtpremium%2F&response_type=code&scope=identify) OR [support server ](https://discord.com/oauth2/authorize?client_id=828285107696697404&redirect_uri=https%3A%2F%2Fdiscord.gg%2FVkpv7pPRMh&response_type=code&scope=identify) Oauth2 link to prevent raid`)
     .setColor("RANDOM")
     .setFooter(`BOT MADE BY crt-romain78#3014`)
     .setTimestamp(message.timestamp = Date.now())
