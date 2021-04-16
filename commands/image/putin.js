d6cd957aa9f7fdf3290d9917d74830e97f918e13
crt-romain78 <75182534+crt-romain78@users.noreply.github.com>
2021-04-16 17:41:57 +0200

    added channelinfo

--- commands/image/putin.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-  name: "putin",
-  aliases: [],
-  category: "Image",
-  description: "put the mentioned user avatar in a painting next to putin",
-  usage: "putin <user>",
-};
